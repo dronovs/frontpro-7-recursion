737c4b6d1902712d03d1a849ea933a64dde86f29
dronovs <od270891dss@gmail.com>
2021-09-23 12:47:18 +0300

    init

---
+++ script/script.js
@@ -0,0 +1,13 @@
+let numbersArr = [1,2,3,4,5,6,7];
+
+function sumArrEls (arr, index) {
+    let result = 0;
+    if (arr[index] >= 0 && arr[index] <= arr.length) {
+        result += arr[index] + sumArrEls(arr, index-1);
+    } else {
+        return 0;
+    }
+    return result;
+}
+
+console.log(sumArrEls(numbersArr, 6));
