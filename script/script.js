2fa353e43b88f74a061f0ef455f70e598ae48d59
dronovs <od270891dss@gmail.com>
2021-09-23 15:10:51 +0300

    update

--- script/script.js
+++ script/script.js
@@ -1,13 +1,19 @@
-const numbersArr = [1,2,3,4,5,6,7];
+function getArr () {
+    const arr = [1,2,3,4,5,6,7];
+    return function () {
+        return arr;
+    }
+}
+
+let numbers = getArr();
 
 function sumArrEls (arr, index) {
     let result = 0;
-    if (arr[index] >= 0 && arr[index] <= arr.length) {
-        result += arr[index] + sumArrEls(arr, index-1);
+    if (index) {
+        return (result += arr[index] + sumArrEls(arr, index - 1));
     } else {
-        return 0;
+        return (result += arr[index]);
     }
-    return result;
 }
 
-console.log(sumArrEls(numbersArr, 6));
+console.log(sumArrEls(numbers(), 6));
